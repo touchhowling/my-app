f14f464473ec9f0ef1b325f3cb8ac437bc935179
touchhowling <devsaransujan@gmail.com>
2023-07-03 12:00:11 +0530

    Initialise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Navbar = ({ handleGetUsers }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="navbar">
+      <div className="logo">Company Name</div>
+      <div className="buttons">
+        <button className="get-users-button" onClick={handleGetUsers}>
+          Get Users
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+const UserGrid = ({ users }) => {
+  return (
+    <div className="user-grid">
+      {users.map((user) => (
+        <div key={user.id} className="user-card">
+          <img src={user.avatar} alt={user.first_name} />
+          <h3>{`${user.first_name} ${user.last_name}`}</h3>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+const LoadingScreen = () => {
+  return <div className="loading-screen">Loading...</div>;
+};
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGetUsers = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://reqres.in/api/users?page=1');
+      const data = await response.json();
+      setUsers(data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Navbar handleGetUsers={handleGetUsers} />
+      <div className="content">
+        {isLoading ? (
+          <LoadingScreen />
+        ) : (
+          users.length > 0 && <UserGrid users={users} />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
